3304ae3628636a1d7896b4111ac09bba225d8ff5
Ashfakul Islam <r4pido3@gmail.com>
2023-11-23 19:21:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "flowbite-react"
+
+function App() {
+  return (
+    <>
+    sadasd
+    <Button color="gray">Gray</Button>
+    <Button color="blue">Blue</Button>
+    </>
+  )
+}
+
+export default App
