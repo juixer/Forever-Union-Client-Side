1e53f9c8125f8e0785cbf885a06af5ee79c9e5dd
Ashfakul Islam <r4pido3@gmail.com>
2023-11-24 20:26:13 +0600

    biodata details page gender wise data showing

--- src/Pages/SingleBioData/SingleBioData.jsx
+++ src/Pages/SingleBioData/SingleBioData.jsx
@@ -1,77 +1,125 @@
 import { Button } from "flowbite-react";
-import BioDataCard from "../../Shared/BioDataCard/BioDataCard";
 import Container from "../../Shared/Container/Container";
 import { FaHeart, FaLock } from "react-icons/fa6";
+import { useLoaderData } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { axiosPublic } from "../../Hooks/useAxiosPublic/useAxiosPublic";
+import BioDataCard from "../../Shared/BioDataCard/BioDataCard";
 
 const SingleBioData = () => {
   const user = true;
+
+  // single bio data details
+  const bioDataInfo = useLoaderData();
+  const {
+    _id,
+    name,
+    profileImage,
+    gender,
+    dateOfBirth,
+    height,
+    weight,
+    age,
+    occupation,
+    race,
+    fathersName,
+    mothersName,
+    expectedPartnerAge,
+    expectedPartnerHeight,
+    expectedPartnerWeight,
+    contactEmail,
+    biodataId,
+    mobileNumber,
+    permanentDivision,
+    presentDivision,
+  } = bioDataInfo;
+
+  // Tan Stack query
+  const {
+    isPending,
+    error,
+    data: matchedData = [],
+  } = useQuery({
+    queryKey: ["matchedData"],
+    queryFn: async () => {
+      const res = await axiosPublic.get(`/biodatas/gender/${gender}`);
+      return res.data;
+    },
+  });
+
+  // filter
+  const filterMatchedData = matchedData.filter((data) => data._id !== _id);
   return (
     <Container>
       <div className="my-5 flex lg:flex-row flex-col gap-10">
         <div className="lg:w-[60%]">
           <h1 className="text-center md:text-5xl text-2xl font-bold my-10">
-            Maharob Hossain Details
+            BioData ID-{biodataId} Details
           </h1>
           <div className="flex flex-col gap-5">
             {/* box */}
             <div className="flex justify-center items-center gap-16 md:flex-row flex-col">
-              <img
-                src="https://i.ibb.co/jDMGShY/rainy-1.png"
-                className="full_round w-80"
-              />
+              <img src={profileImage} className="full_round w-80" />
               <div className="flex flex-col gap-3">
-                <h1 className="md:text-5xl text-3xl font-light">
-                  Maharob Hossain
-                </h1>
-
-                <h1 className="md:text-3xl text-xl font-light">Gender: Male</h1>
+                <h1 className="md:text-5xl text-3xl font-light">{name}</h1>
 
                 <h1 className="md:text-3xl text-xl font-light">
-                  Date of Birth: 24-04-1999
+                  Gender: {gender}
                 </h1>
 
-                <h1 className="md:text-3xl text-xl font-light">Age: 24 Y/O</h1>
-
-                <h1 className="md:text-3xl text-xl font-light">Race: Islam</h1>
+                <h1 className="md:text-3xl text-xl font-light">
+                  Date of Birth: {dateOfBirth}
+                </h1>
 
                 <h1 className="md:text-3xl text-xl font-light">
-                  Occupation: Student
+                  Age: {age} Y/O
+                </h1>
+
+                <h1 className="md:text-3xl text-xl font-light">
+                  Race: {race}{" "}
+                </h1>
+
+                <h1 className="md:text-3xl text-xl font-light">
+                  Occupation: {occupation}
                 </h1>
               </div>
             </div>
             {/* BOX */}
             <div className="grid grid-cols-1 md:grid-cols-2 gap-y-5 ">
               <h1 className="md:text-3xl text-xl font-light">
-                Father Name: Naruto Uzumaki
+                Father Name: {fathersName}
               </h1>
 
               <h1 className="md:text-3xl text-xl font-light">
-                Mother Name: Hinata Uzumaki
-              </h1>
-
-              <h1 className="md:text-3xl text-xl font-light">Height: 6ft</h1>
-
-              <h1 className="md:text-3xl text-xl font-light">Weight: 75kg</h1>
-
-              <h1 className="md:text-3xl text-xl font-light">
-                Permanent Division: Dhaka
+                Mother Name: {mothersName}
               </h1>
 
               <h1 className="md:text-3xl text-xl font-light">
-                Present Division: Khulna
+                Height: {height}
               </h1>
 
               <h1 className="md:text-3xl text-xl font-light">
-                Expected Partner Age: 7 Y/O
+                Weight: {weight}kg
               </h1>
 
               <h1 className="md:text-3xl text-xl font-light">
-                Expected Partner Height: 7inc
+                Permanent Division: {permanentDivision}
               </h1>
 
               <h1 className="md:text-3xl text-xl font-light">
-                Expected Partner Weight:
-                <span className="font-light text-2xl"> 150kg </span>
+                Present Division: {presentDivision}
+              </h1>
+
+              <h1 className="md:text-3xl text-xl font-light">
+                Expected Partner Age: {expectedPartnerAge} Y/O
+              </h1>
+
+              <h1 className="md:text-3xl text-xl font-light">
+                Expected Partner Height: {expectedPartnerHeight}
+              </h1>
+
+              <h1 className="md:text-3xl text-xl font-light md:col-span-2">
+                Expected Partner Weight: {expectedPartnerWeight}kg
               </h1>
             </div>
             {/* box */}
@@ -91,20 +139,22 @@ const SingleBioData = () => {
                 <div className="flex flex-col justify-center items-center gap-5 mt-5">
                   <h1 className="md:text-3xl text-xl font-light">
                     Email:{" "}
-                    <span className="underline text-2xl">
-                      shantoking3@gmail.com
-                    </span>
+                    <span className="underline text-2xl">{contactEmail}</span>
                   </h1>
                   <h1 className="md:text-3xl text-xl font-light">
                     Phone:
-                    <span className="text-2xl"> 0179199005</span>
+                    <span className="text-2xl"> {mobileNumber}</span>
                   </h1>
                 </div>
               ) : (
                 <div className="flex flex-col justify-center items-center gap-3">
-                  <img src="https://i.ibb.co/cY64wtN/contactlocked.png" className="w-44"/>
-                  <h1 className="font-semibold text-xl">BioData Contact Information Locked
-                      Pay 500TK to Get Access of the Contact
+                  <img
+                    src="https://i.ibb.co/cY64wtN/contactlocked.png"
+                    className="w-44"
+                  />
+                  <h1 className="font-semibold text-xl">
+                    BioData Contact Information Locked Pay 500TK to Get Access
+                    of the Contact
                   </h1>
                   <Button
                     gradientMonochrome="lime"
@@ -118,10 +168,10 @@ const SingleBioData = () => {
           </div>
         </div>
         <div className="lg:w-[40%]">
-          <div className="flex flex-col gap-10">
-            <BioDataCard />
-            <BioDataCard />
-            <BioDataCard />
+          <div className="flex flex-col gap-y-10">
+            {filterMatchedData.map((data) => (
+              <BioDataCard key={data._id} data={data} />
+            ))}
           </div>
         </div>
       </div>
